32e05247216c9d45a2e2a9528686cd51cd78ccb1
SangMeeSpecht <[01;31m[K"email"[m[K: null>
2016-08-09 23:30:30 -0500

    Init file and add challenge description

---
+++ ruby-quiz-154-make-change.rb
@@ -0,0 +1,5 @@
+# Ruby Quiz 154: Make Change (http://rubyquiz.com/quiz154.html)
+# Create a function that when given a certain change amount, will return the optimal change (i.e. the least amount of coins involved)
+# Assume that you have an infinite number of coins to work with
+# Using USA currency (.25, .10, .05, .01)
+
